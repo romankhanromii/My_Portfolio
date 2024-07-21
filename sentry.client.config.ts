bc14156074640b7b5ba3454919db450420e9b31e
romankhanromii <romankhanrk224@gmail.com>
2024-07-21 21:58:08 +0500

    my portfolio bug fix

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://32e2585114eafaaf33f4f82885c9994d@o4507640760762368.ingest.de.sentry.io/4507640765087824",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -25,6 +25,10 @@ Sentry.init({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
       blockAllMedia: true,
+    }),
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "dark",
     }),
   ],
 });
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://32e2585114eafaaf33f4f82885c9994d@o4507640760762368.ingest.de.sentry.io/4507640765087824",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
