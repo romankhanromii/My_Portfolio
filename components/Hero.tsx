bccdcf6f0771eb25f99bbfd9b570b77d61c337b6
romankhanromii <romankhanrk224@gmail.com>
2024-07-22 09:56:46 +0500

    my portfolio bug fix small

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,93 @@
-import { FaLocationArrow } from "react-icons/fa6";
+// import { FaLocationArrow } from "react-icons/fa6";
 
+// import MagicButton from "./MagicButton";
+// import { Spotlight } from "./ui/Spotlight";
+// import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+// const Hero = () => {
+//   return (
+//     <div className="pb-20 pt-36">
+//       {/**
+//        *  UI: Spotlights
+//        *  Link: https://ui.aceternity.com/components/spotlight
+//        */}
+//       <div>
+//         <Spotlight
+//           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+//           fill="white"
+//         />
+//         <Spotlight
+//           className="h-[80vh] w-[50vw] top-10 left-full"
+//           fill="purple"
+//         />
+//         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+//       </div>
+
+//       {/**
+//        *  UI: grid
+//        *  change bg color to bg-black-100 and reduce grid color from
+//        *  0.2 to 0.03
+//        */}
+//       <div
+//         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+//        absolute top-0 left-0 flex items-center justify-center"
+//       >
+//         {/* Radial gradient for the container to give a faded look */}
+//         <div
+//           // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+//           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+//          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+//         />
+//       </div>
+
+//       <div className="flex justify-center relative my-20 z-10">
+//         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+//           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+//             Dynamic Web Magic with Mern Stack
+//           </p>
+
+//           {/**
+//            *  Link: https://ui.aceternity.com/components/text-generate-effect
+//            *
+//            *  change md:text-6xl, add more responsive code
+//            */}
+//           <TextGenerateEffect
+//             words="Crafting Dynamic Web Solutions with MERN Stack"
+//             className="text-center text-[40px] md:text-5xl lg:text-6xl"
+//           />
+
+//           {/* <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+//             Hi! I&apos;m Roman Khan, a Mern Stack Developer based in Pakistan.
+//           </p> */}
+//           <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+//             <img
+//               className=" mx-auto my-5 w-40 h-40 object-cover rounded-[50%] "
+//               src="/myimage2.jpg"
+//               alt="Profile Picture"
+//             />
+//             <div className="px-6 py-4">
+//               <p className="text-center tracking-wide mb-4 text-sm md:text-lg lg:text-2xl">
+//                 Hi! I'm Roman Khan, a MERN Stack Developer based in Pakistan.
+//               </p>
+//             </div>
+//           </div>
+
+//           <a href="#about">
+//             <MagicButton
+//               title="Show my work"
+//               icon={<FaLocationArrow />}
+//               position="right"
+//             />
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hero;
+import { FaLocationArrow } from "react-icons/fa6";
+import Image from "next/image";
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
@@ -34,7 +122,7 @@ const Hero = () => {
       >
         {/* Radial gradient for the container to give a faded look */}
         <div
-          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+          // change the bg to bg-black-100, so it matches the bg color and will blend in
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
@@ -60,14 +148,17 @@ const Hero = () => {
             Hi! I&apos;m Roman Khan, a Mern Stack Developer based in Pakistan.
           </p> */}
           <div className="max-w-sm rounded overflow-hidden shadow-lg ">
-            <img
-              className=" mx-auto my-5 w-40 h-40 object-cover rounded-[50%] "
+            <Image
+              className="mx-auto my-5 w-40 h-40 object-cover rounded-[50%]"
               src="/myimage2.jpg"
               alt="Profile Picture"
+              width={160}
+              height={160}
             />
             <div className="px-6 py-4">
               <p className="text-center tracking-wide mb-4 text-sm md:text-lg lg:text-2xl">
-                Hi! I'm Roman Khan, a MERN Stack Developer based in Pakistan.
+                Hi! I&apos;m Roman Khan, a MERN Stack Developer based in
+                Pakistan.
               </p>
             </div>
           </div>
